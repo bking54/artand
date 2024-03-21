0ce5152b05701634551a0398f45cf60f12ab957d
Brad King <bking5194@gmail.com>
2024-03-21 15:37:41 -0400

    email config

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -216,18 +216,18 @@ exports.pay = (req, res/*, next*/) => {
     console.log(data);
 
     //Email Confirmation
-    //Send to client and business
-
+    //create html
     let clientHTML = '';
 
-    let archiveHTML = '';
+    let archiveHTML = '<html> <head> <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> <style> h1, h2, h3, p { font-family: "Roboto"; } h1 { font-weight: 420; } h2 { font-weight: 400; } table { font-family: "Roboto", sans-serif; border-collapse: collapse; width: 100%; border: #000d4e; } td, th { border: 1px solid #000d4e; text-align: left; padding: 8px; } tr:nth-child(odd) { background-color: #ccd2ef; } .tables { margin: 4vw; } </style> </head> <body style="margin: 0%; padding: 0%;"> <header style="position: absolute; background-color: #000d4e; height: auto; margin: 0; padding: 0; border-bottom: 0.25vw solid #faad3f; width: 100%;"> <h1 style="color: #fff; text-align: center;">Math Art Connections</h1> <div style="background-color: hsl(230, 40%, 35%);"> <h2 style="color:#fff; text-align: center; margin: 0%; padding: .6vw 0% .6vw 0%;">Registration Information For </h2> </div> </header> <div style="position: absolute; min-height: 100vh; top: 25%;"> <div style="padding-bottom: 2.5em;"> <div class="tables"> <div id="parents"> <h3>Parent Information</h3> <table> <tr> <th>First Name</th> <th>Last Name</th> <th>Phone Number</th> <th>Email Address</th> <th>Address</th> <th>Apt. Number</th> <th>City</th> <th>State</th> <th>Zip Code</th> </tr> <tr id="parent1"> </tr> <tr id="parent2"> </tr> </table> </div> <div id="emergency"> <h3>Emergency Contact</h3> <table> <tr> <th>First Name</th> <th>Last Name</th> <th>Relationship to Child</th> <th>Cell phone</th> <th>Home phone</th> <th>Work Phone</th> </tr> <tr id="emergency-contact"> </tr> </table> </div> <div id="pickup"> <h3>Approved Pickup List</h3> <table> <tr> <th>Full Name</th> <th>Relationship to Child</th> </tr> <tr id="pickup1"> </tr> <tr id="pickup2"> </tr> <tr id="pickup3"> </tr> </table> </div> <div id="children"> <h3>Camper Information</h3> <table> <tr> <th>First Name</th> <th>Last Name</th> <th>Birthday</th> <th>Grade</th> <th>Important Information</th> </tr> <tr class="child1"> </tr> <tr class="child2"> </tr> <tr class="child3"> </tr> <tr class="child4"> </tr> </table> </div> <div id="bill"> <h3>Enrollment and Billing Information</h3> <table> <tr> <th>Camper</th> <th>Plus Minus Zero</th> <th>Times Into Fractions</th> <th>Geometry</th> <th>Tubro Multiplication</th> </tr> <tr class="child1"> </tr> <tr class="child2"> </tr> <tr class="child3"> </tr> <tr class="child4"> </tr> </table> </div> </div> </div> </div> <footer> </footer> </body></html>';
 
+    //Send to client and business
     let clientMailOptions = {
         from: {
             name: 'Artand - Math Art Connections',
             address: 'bking5194@gmail.com'
         },
-        to: 'bking5194@gmail.com',
+        to: parent1.email1,
         subject: 'Math Art Connections Registration',
         text: 'This is just a test'
     };
@@ -239,7 +239,7 @@ exports.pay = (req, res/*, next*/) => {
         },
         to: 'bking5194@gmail.com',
         subject: 'New Registration!',
-        text: ''
+        html: archiveHTML
     };
 
     const sendToCleint = async (transporter, clientMailOptions) => {
@@ -261,7 +261,7 @@ exports.pay = (req, res/*, next*/) => {
 
     //uncomment in production
     //sendToClient(transporter, clientMailOptions);
-    //sendToArchive(transporter, mailOptions);
+    sendToArchive(transporter, mailOptions);
 
     //paypal magic
     //NOT IN THIS VERSION
